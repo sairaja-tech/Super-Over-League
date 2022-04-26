3835441bdf5c83737e38ecb8bfc0c57890b78608
Sairja <k.sairaja2000@gmail.com>
2022-04-26 12:59:24 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const element = (
+  <div class="bgContainer">
+    <h1 class="heading">Super Over League</h1>
+    <div>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+        class="image"
+      />
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+        class="image"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
